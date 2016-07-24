76ca6ada62855c781c2cd4c794475c7ad1789c95
Hao Men <hao.men@gmail.com>
2016-07-23 22:02:22 -0400

    update with working copy of bus points, pop info window and route 57st layer

--- generateGeojson.py
+++ generateGeojson.py
@@ -3,25 +3,37 @@
 import sys
 import MySQLdb
 if __name__=="__main__":
-    if len(sys.argv)!=2:
-        print 'should be\n'+sys.argv[0]+' <sql select query>'
+    if len(sys.argv)!=3:
+        print 'should be\n'+sys.argv[0]+' <sql select query> <output.json>'
         sys.exit(1)
 
     sql_query=sys.argv[1]
-    of=open('test33p_js.geojson','w')
+    of=open(sys.argv[2],'w')
 
     # write top header
-    of.write('''data={\n"type":"FeatureCollection",\n''')
+    of.write('''{\n"type":"FeatureCollection",\n''')
     of.write('''"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },\n\n"features": [\n''')
 
     db=MySQLdb.connect('localhost','nywaterway','nywaterway','nywaterway')
     cursor=db.cursor()
     cursor.execute(sql_query)
     #print cursor.rowcount
-    for i in range(cursor.rowcount):
+    for i in range(cursor.rowcount-1):
         row=cursor.fetchone()
+        route_qid=str(row[1])
+        busid=str(row[2])
         lon=str(row[3])
         lat=str(row[4])
-        of.write('''{ "type": "Feature", "geometry": { "type": "Point", "coordinates": ['''+lon+','+lat+''']}},\n''')
+        orientation=str(row[5])
+        of.write('''{ "type": "Feature", "properties":{"route_qid":'''+route_qid+", \"bus_id\":"+busid+",\"orientation\":"+orientation+'''}, "geometry": { "type": "Point", "coordinates": ['''+lon+','+lat+''']}},\n''')
+
+    row=cursor.fetchone()
+    route_qid=str(row[1])
+    busid=str(row[2])
+    lon=str(row[3])
+    lat=str(row[4])
+    orientation=str(row[5])
+    of.write('''{ "type": "Feature", "properties":{"route_qid":'''+route_qid+", \"bus_id\":"+busid+",\"orientation\":"+orientation+'''}, "geometry": { "type": "Point", "coordinates": ['''+lon+','+lat+''']}}\n''')
+
     of.write(']\n}\n')
     of.close()
