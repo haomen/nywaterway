2d0ec99067ca87b6877869c73e35771a649677e3
Hao Men <hao.men@gmail.com>
2016-07-27 17:36:59 -0400

    add auto bus location fetch, isolate bus location fetching with front end refresh, so total google api consumption is stable per day, use internal map cache to restore bus locations, key as bus id

--- nywBusREST.py
+++ nywBusREST.py
@@ -1,29 +1,96 @@
 #!/usr/bin/env python
 
+# 2 threads running at the same time:
+# Thread #1:
+# query routes and get positions into a global dictionary
+#
+# Thread #2:
+# collect data from current position pool and return as geojson format to http query
+
 from flask import Flask, request
 from flask_restful import Resource, Api
 from flask.ext.jsonpify import jsonify
 from nywBusPos import nywBusPos
-import json
+import json,thread,time,datetime
+
 app = Flask(__name__)
 api = Api(app)
 
-todos = {}
+bus_pool={}
+interval=5
+route_ids=[1,21,22,32,43]
 
-class NywBusLocation(Resource):
+def busPos2GeoJson(bus_pos):
+    bus_gj={}
+    bus_gj.update({"type":"FeatureCollection"})
+    bus_gj.update({"crs":
+                   { "type": "name",
+                     "properties":
+                     { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" }
+                   }
+    })
+    bus_gj.update({"features":[]})
+    for k,v in bus_pos.iteritems():
+        bus_point={
+            "geometry":{
+                "coordinates":[
+                    v["lng"],v["lat"]
+                ],
+                "type":"Point"
+            },
+            "properties":{
+                "bus_id":k,
+                "orientation":v["ori"],
+                "route_qid":v["bid"],
+                "last_update":v["last_update"]
+            },
+            "type":"Feature"
+        }
+        bus_gj["features"].append(bus_point)
+    return bus_gj
+
+def getAllBusLocations():
+    #init route id depend on time
+    #query buslocation from all route id,get all bus location at this time
+    #update pos_pool
+    while True:
+        print 'refresh bus locations at:\t'+str(datetime.datetime.now())
+        for route_id in route_ids:
+            #print route_id
+            a_route=nywBusPos(route_id)
+            bus_pos_on_route=a_route.queryBusGPSPositionOnRoad()
+            bus_pos_item={}
+            for bus_pos in bus_pos_on_route:
+                #print bus_pos
+                bus_pos_item.update({
+                    bus_pos["bus_id"]:
+                    {
+                        "lat":bus_pos["gps_y"],
+                        "lng":bus_pos["gps_x"],
+                        "ori":bus_pos["orientation"],
+                        "bid":route_id,
+                        "last_update":datetime.datetime.now()
+                    }
+                })
+            bus_pool.update(bus_pos_item)
+        time.sleep(interval)
+
+class NywBusPosService(Resource):
     def get(self, route_id):
-        a_route=nywBusPos(int(route_id))
-        #return 'callback('+json.dumps(a_route.queryBusGPSPositionORGeojson())+');'
-        #return '{0}({1})'.format('callback',json.dumps(a_route.queryBusGPSPositionORGeojson()))
-        s1=jsonify(a_route.queryBusGPSPositionORGeojson())
-        return s1
+        #a_route=nywBusPos(int(route_id))
+        #return jsonify(a_route.queryBusGPSPositionORGeojson())
+        print len(bus_pool)
+        return jsonify(busPos2GeoJson(bus_pool))
 
-    # leave the put method exmpty for now, only GET
-    # def put(self, todo_id):
-    #     todos[todo_id] = request.form['data']
-    #     return {todo_id: todos[todo_id]}
-
-api.add_resource(NywBusLocation, '/nywaterway/<string:route_id>')
 
 if __name__ == '__main__':
+    # start web server
+    api.add_resource(NywBusPosService, '/nywaterway/<string:route_id>')
+    thread.start_new_thread(getAllBusLocations,())
     app.run(host='0.0.0.0',port=30444,debug=True)
+    # while True:
+    #     pass
+    # a_map=NywBusQueryPos()
+    # a_map.getAllBusLocations()
+    # js=busPos2GeoJson(bus_pool)
+    # print js
